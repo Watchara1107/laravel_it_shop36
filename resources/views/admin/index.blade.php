22548e9934e6644498371f69207438a7755ac45f
Watchara Ketkaew <wk996110@gmailcom>
2021-08-26 12:34:41 +0700

    update2  26-8-2564

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,35 +1,38 @@
-@extends('layouts.admin.admin')
-@section('body')
-<div class="container-fluid">
-    <div class="row mt-4">
-        <h2>Product</h2>
-        <table class="table">
-            <thead class="thead-dark">
-                <tr>
-                    <th scope="col">ID</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Image</th>
-                    <th scope="col">Category</th>
-                    <th scope="col">Description</th>
-                    <th scope="col">Price</th>
-                    <th scope="col">Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                    <td>@mdo</td>
-                    <td>@mdo</td>
-                    <td>
-                        <a href="" class="btn btn-success">Edit</a>
-                        <a href="" class="btn btn-danger">Delete</a>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
-</div>
-@endsection
+ @extends('layouts.admin.admin')
+ @section('conten')
+
+ <div class="container">
+     <div class="row mt-4">
+         <h2>Products</h2>
+         <table class="table">
+             <thead class="thead-dark">
+                 <tr>
+                     <th scope="col">ID</th>
+                     <th scope="col">Name</th>
+                     <th scope="col">Image</th>
+                     <th scope="col">Category</th>
+                     <th scope="col">Description</th>
+                     <th scope="col">Price</th>
+                     <th scope="col">Action</th>
+                 </tr>
+             </thead>
+             <tbody>
+                 <tr>
+                     <th scope="row">1</th>
+                     <td>Mark</td>
+                     <td>Otto</td>
+                     <td>@mdo</td>
+                     <td>@mdo</td>
+                     <td>@mdo</td>
+                     <td>
+                         <a href="" class="btn btn-success">Edit</a>
+                         <a href="" class="btn btn-danger">Delete</a>
+                     </td>
+                 </tr>
+
+             </tbody>
+         </table>
+     </div>
+ </div>
+
+ @endsection
